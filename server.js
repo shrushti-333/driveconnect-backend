134d048298400a9ac2d03cfe34315f1e10bdb2e6
shrushti-333 <shrushtisiriya33@gmail.com>
2026-03-25 14:32:25 +0530

    Add Netlify frontend URL to CORS

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://melodic-centaur-586771.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
