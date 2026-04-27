f6178dda3752634fcb5c82251772516399cc352c
shrushti-333 <shrushtisiriya33@gmail.com>
2026-04-27 21:16:46 +0530

    final render fix

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // Start Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`📍 Environment: ${process.env.NODE_ENV}`);
 });
